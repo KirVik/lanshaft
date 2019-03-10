415907a6e1df343c21c4f0bbfc0277f7a16bff11
KirVik <Kirich021@gmail.com>
2019-03-10 23:35:10 +0200

    гамбургер

--- js/main.js
+++ js/main.js
@@ -6,4 +6,19 @@ $(function (){
         // prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
         // nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>',
     });
+    
+    // гамбургер
+    $('.menu-open').on("click", function (event) {
+        event.stopPropagation();
+        $('.menu-collapse').toggleClass('d-none');
+        $(".menu, li .top-menu__item").toggleClass("menu-opened");
+    });
+
+    $(document).on('click', function (event) {
+        if (!$(event.target).closest('.menu-open').length) {
+            $(".menu, li .top-menu__item").removeClass("menu-opened");
+            $('.menu-collapse').addClass('d-none');
+
+        };
+    });
 })
