d85a9af6e30e072dd83402a2ccb430ba20036356
RG3 <42303583+KirVik@users.noreply.github.com>
2019-03-10 22:19:45 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function (){
+    $('.slider').slick({
+        // infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
+        // nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>',
+    });
+})
